2168f4c04cacd85acb93ffec26d925b62255252e
Davi Nascimento <francisco.nascimento1908@aluno.ce.gov.br>
2025-05-16 08:43:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       
       <Profile nome="Amanda Mendes" idade={21} profissao="Atora" foto="https://i.pinimg.com/originals/0d/39/24/0d39245b27c24333fe9356b788ce8feb.jpg" />
       <Profile nome="Juliana Costa" idade={17} profissao="Engenheira" foto="https://tse1.mm.bing.net/th/id/OIP.RskHY1i8sOqpHZD_hr3hKwHaHa?rs=1&pid=ImgDetMain" />
-      <Profile nome="Davi Delas" idade={16} profissao="Desenvolvedoer" foto="https://avatars.githubusercontent.com/u/178114108?v=4&size=64" />
+      <Profile nome="Davi Delas" idade={16} profissao="Desenvolvedor" foto="https://avatars.githubusercontent.com/u/178114108?v=4&size=64" />
     </div>
   );
 };
